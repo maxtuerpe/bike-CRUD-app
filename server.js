3b576299dbd0574d60bb079c904b8d032c512aa0
Max Tuerpe <maxtuerpe4@gmail.com>
2018-10-19 12:00:03 -0600

    added auth view, auth controller, users model

--- server.js
+++ server.js
@@ -7,6 +7,33 @@ const morgan         = require('morgan');
 // CONTROLLERS
 const authController = require('./controllers/auth');
 
+require('./db/db');
+
+app.use(session({
+    secret: 'This is some random secret string',
+    resave: false,
+    saveUninitialized: false
+}));
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
+
+
+app.use('/auth', authController);
+
+
+
+app.listen(3000, () => {
+    console.log('server running on port 3000');
+})
+const app            = express();
+const bodyParser     = require('body-parser');
+const methodOverride = require('method-override');
+const session        = require('express-session');
+const morgan         = require('morgan');
+// CONTROLLERS
+const authController = require('./controllers/auth');
+
 
 
 app.use(session({
