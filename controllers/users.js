e37eee00ca11e1982a3053ab2ac2c4e0b23c22c5
Max Tuerpe <maxtuerpe4@gmail.com>
2018-10-20 14:29:18 -0600

    built models, user index get route

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users');
+const Post = require('../models/posts');
+const Comment = require('../models/comments');
+
+
+router.get('/', async(req,res) => {
+    try{
+
+    }catch(err){
+        res.send(err);
+    }
+})
 
 
 
