4bb5241759bbf9be7b3c05b0730e7f85808b3779
Max Tuerpe <maxtuerpe4@gmail.com>
2018-10-20 13:54:15 -0600

    views files

--- models/comments.js
+++ models/comments.js
@@ -1,1 +1,6 @@
 const mongoose = require('mongoose');
+
+const commentsSchema = new mongoose.Schema ({
+    user: {type: mongoose.Schema.Types.ObjectId, require: 'User'},
+    comment: {}
+})
--- models/posts.js
+++ models/posts.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const postsSchema = new mongoose.Schema ({
-    user: {type: mongoose.Schema.Types.ObjectId, required: "User"},
-    
+    user: {type: mongoose.Schema.Types.ObjectId, require: "User"},
+    comments: {type: mongoose.Schema.Types.ObjectId, require: 'Comment'}
 })
+
+module.exports = mongoose.model('Post', postsSchema);
