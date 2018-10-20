a9f9ee60eabc6b845409f810c5df2ea88cad08b2
James <tytechortz@gmail.com>
2018-10-20 13:54:36 -0600

    starts work on posts controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,3 +2,28 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/posts')
 const User = require('../models/users')
+
+
+router.get('/', (req, res) => {
+    Post.find({}, (err, foundPosts) => {
+        res.render('posts/index.ejs', {
+            posts: foundPosts
+        });
+    });
+});
+
+router.get('/new', (req, res) => {
+    User.find({}, (err, allUsers) => {
+        res.render('posts/new.ejs', {    
+        });
+    });
+});
+
+
+
+
+
+
+module.exports = router;
+
+
